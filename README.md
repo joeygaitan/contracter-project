0d95cc834c0fd6bf28e5303acac6dfdc5cbd12f7
Joey Gaitan <joeygaitan707@gmail.com>
2019-10-09 17:59:43 -0700

    setting up file structure

---
+++ README.md
@@ -0,0 +1,12 @@
+## Contracter Project
+
+## User Stories
+1. As a user I can sign up
+2. As a user I can login
+
+1. I can add items to my indiviual cart
+2. I can remove items from my cart
+3. I can modify my cart items
+4. I can see my exact cart items
+
+
--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+# client = MongoClient()
+# db = client.Contracter
+
+# client = MongoClient()
+
+items = [{"image": "../assets/shirts/blueWhiteShirt.png","title":"Blue and White Shirt", "description": "This shirt is Blue and White. It comes in many sizes. Theses shirts that sold here are all unisex. please pick accordingly","price": 10.00, "size": ["xs","s","m","l","xl"]}
+,{"image": "../assets/shirts/fendiShirt.png","title":"Fendi Shirt", "description": "This shirt is black and has a white Fendi text on it. It comes in many sizes. Theses shirts that sold here are all unisex. please pick accordingly", "size": ["xs","s","m","l","xl"], "price": 20.00}
+,{"image": "../assets/shirts/focusShirt.png","title":"Focus Shirt", "description": "This shirt is black Focus text on it. It comes in many sizes. Theses shirts that sold here are all unisex. please pick accordingly","size": ["xs","s","m","l","xl"], "price": 25.00}
+,{"image": "../assets/shirts/focusShirt.png","title":"Pink Shirt", "description": "This shirt is pink. It comes in many sizes. Theses shirts that sold here are all unisex. please pick accordingly", "size": ["xs","s","m","l","xl"], "price": 15.00},
+{"image": "../assets/shirts/","title":"", "description": ""}]
 
 @app.route('/')
 def index():
     """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
+    return render_template('home.html', items=items)
+
+if __name__ == "__main__":
+    app(debug=True)
