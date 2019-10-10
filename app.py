fa377598e578f8e8734db6d511e0cc26cd8102af
Joey Gaitan <joeygaitan707@gmail.com>
2019-10-10 02:35:01 -0700

    added in first mongodb insert to database. I installed catalina. Big mistake. I might have to change root directory of file to another So I can run my mongodb

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ items = [{"image": "/blueWhiteShirt.png","title":"Blue and White Shirt", "descri
 ,{"image": "/focusShirt.png","title":"Focus Shirt", "description": "This shirt is black Focus text on it. It comes in many sizes. Theses shirts that sold here are all unisex. please pick accordingly","size": ["xs","s","m","l","xl"], "price": 25.00,"-id":2}
 ,{"image": "/pinkShirt.png","title":"Pink Shirt", "description": "This shirt is pink. It comes in many sizes. Theses shirts that sold here are all unisex. please pick accordingly", "size": ["xs","s","m","l","xl"], "price": 15.00,"-id":3},
 {"image": "/whiteShirt.png","title":"White Shirt", "description": "This shirt is white. It comes in many sizes. Theses shirts that sold here are all unisex. please pick accordingly", "size": ["xs","s","m","l","xl"], "price": 10.00,"-id":4},
-{"image": "/circleShirt.png","title":"Pink Shirt", "description": "This shirt is has a orange cirle. It comes in many sizes. Theses shirts that sold here are all unisex. please pick accordingly", "size": ["xs","s","m","l","xl"], "price": 20.00,"-id":5},
+{"image": "/circleShirt.png","title":"Design Shirt", "description": "This shirt is has a orange cirle. It comes in many sizes. Theses shirts that sold here are all unisex. please pick accordingly", "size": ["xs","s","m","l","xl"], "price": 20.00,"-id":5},
 {"image": "/yellowStripeShirt.png","title":"Yellow Stripe Shirt", "description": "This shirt is has a nice yellow stripe on it. It comes in many sizes. Theses shirts that sold here are all unisex. please pick accordingly", "size": ["xs","s","m","l","xl"], "price": 16.00,"-id":6},
 {"image": "/beastShirt.png","title":"Beast Shirt", "description": "This is a yellow shirt with the word Beast on it. It comes in many sizes. Theses shirts that sold here are all unisex. please pick accordingly", "size": ["xs","s","m","l","xl"], "price": 11.00,"-id":7}]
 
@@ -30,10 +30,26 @@ def shirt(id):
     return render_template('item.html', item=item)
 
 @app.route('/cart/add/<id>')
-def create_cart(id):
+def create_cart_item(id):
     id = int(id)
     item = items[id]
     return render_template('create_cart_item.html', item=item)
+
+@app.route('/', methods=['POST'])
+def cart_submit():
+    id = request.form.get('id')
+    id = int(id)
+    item = items[id]
+    """Submit a new playlist."""
+    cart_item = {
+        'quantity': request.form.get('quantity'),
+        'size': request.form.get('size'),
+        'image': item['image'],
+        'title': item['title'],
+        'price': item['price'] * int(request.form.get('quantity'))
+    }
+    cart.insert_one(cart_item)
+    return redirect(url_for('home.html'))
 
 
 if __name__ == "__main__":
